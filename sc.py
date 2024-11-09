5f637b3afe54df5d9bb819d6b24b8ba9e371f0eb
lankfwe <fredycortez608@gmail.com>
2024-11-09 13:46:31 -0600

    made neater

--- sc.py
+++ sc.py
@@ -3,14 +3,14 @@ import pandas as pd
 # Read in csv files for each month
 # store in a df
 print ("starting read: ")
-april_df = pd.read_csv("april_2024.csv")
-print ("done with april")
-may_df = pd.read_csv("may_2024.csv")
-june_df = pd.read_csv("june_2024.csv", encoding = 'latin1') # latin 1 prevents read errors in june
-july_df = pd.read_csv("july_2024.csv")
-august_df = pd.read_csv("august_2024.csv")
-september_df = pd.read_csv("september_2024.csv")
-october_df = pd.read_csv("october_2024.csv")
+april_df = pd.read_csv("data/april_2024.csv")
+print ("data/done with april")
+may_df = pd.read_csv("data/may_2024.csv")
+june_df = pd.read_csv("data/june_2024.csv", encoding = 'latin1') # latin 1 prevents read errors in june
+july_df = pd.read_csv("data/july_2024.csv")
+august_df = pd.read_csv("data/august_2024.csv")
+september_df = pd.read_csv("data/september_2024.csv")
+october_df = pd.read_csv("data/october_2024.csv")
 
 # Combine all dfs into a single csv
 combined_df = pd.concat([april_df, may_df, june_df, july_df, august_df, september_df, october_df], ignore_index= False)
