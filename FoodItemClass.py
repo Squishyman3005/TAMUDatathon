62a5a139a9166bd0fb9325c61a624142ecfb8cbe
Sebastian Garcia <sebastian.segarcia@gmail.com>
2024-11-09 15:27:36 -0600

    Creates FoodItemClass

---
+++ FoodItemClass.py
@@ -0,0 +1,28 @@
+class FoodItem:
+    def __init__(self, name):
+        self.name = name # name of the food item
+        self.modifiers = {} # modifiers to the food item
+        self.modifiergroups = {}
+
+    def addModifiers(self, modifiers, modifier):
+        # Takes a food item and modifier as parameter
+        # If modifier not in the dictionary, initialize it
+        if (modifier not in modifiers.keys()):
+            modifiers[modifier] = 1
+        else:
+            modifiers[modifier] += 1
+    def addModifierGroup(self, modifiergroups, modifiergroup):
+        # Takes a modifiergroups and modifiergroup
+        # If modifiergroup not in modifiergroups, initialize it
+        if (modifiergroup not in modifiergroups.keys()):
+            modifiergroups[modifiergroup] =1
+        else: # increment the frequency of modifier group
+            modifiergroups[modifiergroup] += 1
+    def output(self):
+        print (self.name)
+        print (self.modifiers)
+        print (self.modifiergroups)
+        
+
+
+
