b88dc48b96c35154e14a1bc24be69f55b11a6f02
lankfwe <fredycortez608@gmail.com>
2024-11-09 21:04:48 -0600

    Updated functions

--- FoodItemClass.py
+++ FoodItemClass.py
@@ -4,20 +4,20 @@ class FoodItem:
         self.modifiers = {} # modifiers to the food item
         self.modifiergroups = {}
 
-    def addModifiers(self, modifiers, modifier):
+    def addModifiers(self, modifier):
         # Takes a food item and modifier as parameter
         # If modifier not in the dictionary, initialize it
-        if (modifier not in modifiers.keys()):
-            modifiers[modifier] = 1
+        if (modifier not in self.modifiers.keys()):
+            self.modifiers[modifier] = 1
         else:
-            modifiers[modifier] += 1
-    def addModifierGroup(self, modifiergroups, modifiergroup):
+            self.modifiers[modifier] += 1
+    def addModifierGroup(self, modifiergroup):
         # Takes a modifiergroups and modifiergroup
         # If modifiergroup not in modifiergroups, initialize it
-        if (modifiergroup not in modifiergroups.keys()):
-            modifiergroups[modifiergroup] =1
+        if (modifiergroup not in self.modifiergroups.keys()):
+            self.modifiergroups[modifiergroup] =1
         else: # increment the frequency of modifier group
-            modifiergroups[modifiergroup] += 1
+            self.modifiergroups[modifiergroup] += 1
     def output(self):
         print (self.name)
         print (self.modifiers)
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def newOrderIndexes(df):
   listIndexes = []
   orderList = []
   for index, row in df.iterrows():
+    if (index > 400):
+        break
     if (firstRow):
       prev = row[0]
       date = row[1]
@@ -31,8 +33,10 @@ def getFoodIndexes(combined_df):
 
     for index, row in combined_df.iterrows():
         # case for mac and cheese
+        if (index > 400):
+            break
         # if either the next row has noodles or isn't mac and cheese append that index
-        if (index == (len(combined_df)) ):
+        if (index == (len(combined_df) - 2) ):
             break
         if row[4] == "Mac and Cheese":
             if (row[4] != combined_df.iloc[index + 1][4]) or (combined_df.iloc[index+1][3] == "Noods"):
@@ -46,12 +50,8 @@ def getFoodIndexes(combined_df):
             indexes.append(index+1)
         if row[4] == "Mac and Cheese Party Tray (Plus FREE Garlic Bread)":
             indexes.append(index+1)
-        print(index)
-
-
-    indexes += (len.df())
+    indexes += [len(combined_df) - 1]
     return indexes
-
 
 # Various statistical tests
 combined_df = pd.read_csv("combined_sheets_with_june.csv")
@@ -61,9 +61,37 @@ combined_df = pd.read_csv("combined_sheets_with_june.csv")
 #     currentOrder_df = combined_df.iloc(index)
     
 #     addOrder(orderslist, currentOrder_df)
-print(combined_df.dtypes)
-myindexes = getFoodIndexes(combined_df)
-print (myindexes)
+foodsIndex = getFoodIndexes(combined_df)
+orderList, listIndexes = newOrderIndexes(combined_df)
+foods = []
+firstRow = True
+a = FoodItem("")
+counter1 = 1 ; counter2 = 0
+n = 1
+s = 0
+l = 1
+for index, row in combined_df.iterrows():
+    # Need to iterate through rows for order 0 until row index reaches listIndexes[1]
+    # when it reaches listIndexes[1] increment to listIndexes[2] and start orderList[1]
+    # need to iterate for individual food items until index = foodsIndex[1] then increment to foodsIndex[2]
+    # only create a new food item when index == foodsIndex[n]
+    if (index > 400):
+        break
+    
+    if ((index == 0 or index == foodsIndex[n]) and n != len(foodsIndex) - 1):
+        if (index != 0):
+            print(s, index, listIndexes[l], a.output())
+            orderList[s].addFoodItems(a)
+            a = FoodItem(row[4])
+        if (index == 0):
+            a = FoodItem(row[4])
+        if (index == foodsIndex[n]):
+            n += 1
+    a.addModifierGroup(row[3])
+    a.addModifiers(row[2])
+    if (index == listIndexes[l] and l != len(listIndexes)-1):            
+        l += 1
+        s +=1
 
 
 
